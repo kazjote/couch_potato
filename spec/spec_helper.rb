8ea62c741b221d20e22bae8ed07a9eda233ebc41
kazjote <kazjote@ragnarson.com>
2010-08-25 12:52:48 +0200

    Removed hash except dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require 'couch_potato'
 
 require 'time'
 require 'active_support/core_ext/object/blank.rb'
-require 'active_support/core_ext/hash/except.rb'
 
 if ENV["RUN_CODE_RUN"]
   CouchPotato::Config.database_name = 'http://langalex.couch.io/couch_potato_test'
